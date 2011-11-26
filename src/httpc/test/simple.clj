d5e4f4f557aed14aad8b10968ab48882e24105e5
Matti Jagula <matti.jagula@gmail.com>
2011-11-26 22:04:31 +0200

    reduced duplication

--- src/httpc/test/simple.clj
+++ src/httpc/test/simple.clj
@@ -19,6 +19,9 @@
 		 (assert-content (str (converter (op a b))))
 		 :penalty -1))))
 
+(defn- complicate [from to]
+  (if (< (correct-answers) 5) from to))
+
 (defn test-two-numbers-sum [& args]
   ((create-two-number-aritmetic-test +)))
 (defn test-two-numbers-subtract [& args]
@@ -54,7 +57,7 @@
 	       :penalty -1)))
 
 (defn test-largest-number [& args]
-  (let [n (if (< (correct-answers) 5) 5 10)
+  (let [n (complicate 5 10)
 	ns (random-ints n 1 1000)
 	q (str "Which of the numbers is largest: " (str-join ", " ns))]
     (make-test (to-question :params {:q q})
@@ -63,15 +66,16 @@
 	       :penalty -1)))
 
 (defn test-nth-fib [& args]
-  (let [n (random-int 10 (if (< (correct-answers) 5) 30 300))
+  (let [n (random-int 10 (complicate 30 300))
 	f (nth (fibs) n)]
-    (make-test (to-question :params {:q (format "What is the %sth number in Fibonacci sequence" n)})
+    (make-test (to-question :params {:q (format "What is the %sth number in Fibonacci sequence"
+						n)})
 	       (assert-content (str f))
 	       :score 3
 	       :penalty -2)))
 
 (defn test-nth-factorial [& args]
-  (let [n (random-int 3 (if (< (correct-answers) 5) 11 30))
+  (let [n (random-int 3 (complicate 11 30))
 	r (apply * (range 1 (inc n)))]
     (make-test (to-question :params {:q (format "What is the factorial of %s" n)})
 	       (assert-content r)
@@ -79,7 +83,7 @@
 	       :penalty -2)))
 
 (defn test-greatest-common-divisors [& args]
-  (let [[x y] (random-ints 2 3 (if (< (correct-answers) 5) 30 1000000))
+  (let [[x y] (random-ints 2 3 (complicate 30 1000000))
 	r (gcd x y)]
     (make-test (to-question :params {:q (format "What is the greatest common divisor of %s and %s"
 						x y)})
@@ -91,7 +95,7 @@
 	     "Mozilla/5.0 (X11; Linux i686; rv:6.0) Gecko/20100101 Firefox/6.0"
 	     "Mozilla/5.0 (X11; en-US; rv:1.9.2.8) Gecko/20101230 Firefox/3.6.8"
 	     "Googlebot/2.1 (+http://www.googlebot.com/bot.html)"]
-	ua (if (< (correct-answers) 5) (first uas) (rand-nth uas))]
+	ua (complicate (first uas) (rand-nth uas))]
     (make-test (to-question :params {:q "Which browser am I using"}
 			    :headers {"User-Agent" ua})
 	       (assert-content ua)
@@ -103,7 +107,7 @@
 	      "http://www.google.com/search?q=worst%20game%20ever"
 	      "http://en.wikipedia.org/wiki/Representational_state_transfer"
 	      "http://imgur.com/gallery/SkeSE"]
-	rnd-ref (if (< (correct-answers) 5) (first refs) (rand-nth refs))]
+	rnd-ref (complicate (first refs) (rand-nth refs))]
     (make-test (to-question :params {:q "Which page am I coming from"}
 			    :headers {"Referer" rnd-ref})
 	       (assert-content rnd-ref)
